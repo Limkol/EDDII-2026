52f1fb5680a93f07dc398858d22578dd425260d6
Gustavo Francisco Toito <gustavotoito@gmail.com>
2026-08-13 18:02:08 +0000

    inicio da classe aluno

---
+++ Aluno.java
@@ -0,0 +1,13 @@
+public class Aluno {
+    private int ra;
+    private String nome;
+    private String turma;
+
+    public Aluno(int ra, String nome, String turma) {
+        this.ra = ra;
+        this.nome = nome;
+        this.turma =  turma;
+    }
+
+    inserirNota()
+}
