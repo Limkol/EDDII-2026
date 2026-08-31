9a474eecaa3daf9ebe2bb43b18c82779b29bef14
Gustavo Francisco Toito <gustavotoito@gmail.com>
2026-08-31 11:56:47 +0000

    incio arvores binarias

---
+++ Arvores Binarias/main.java
@@ -0,0 +1,18 @@
+import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
+
+class No {
+    int valor;
+    No esquerda:
+    No direita;
+
+    public No(int valor)
+        this.valor = valor;
+}
+
+public main {
+    public static void main(String[] arg) {
+        No
+    }
+}
