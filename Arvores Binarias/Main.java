7f0cfac0139342d8ac79b5bd8876d4768db4c17d
Gustavo Francisco Toito <gustavotoito@gmail.com>
2026-09-06 23:48:34 +0000

    Exercícios árvores binárias 2

---
+++ Arvores Binarias/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        No raiz = new No(50);
+
+        raiz.esquerda = new No(30);
+        raiz.direita = new No(70);
+
+        raiz.esquerda.esquerda = new No(20);
+        raiz.esquerda.direita = new No(40);
+
+        raiz.direita.esquerda = new No(60);
+        raiz.direita.direita = new No(80);
+
+
+        //Teste do diagrama de barras
+        System.out.println("ARVORE ORIGINAL:");
+        Exercicios.diagrama(raiz);
+
+
+        //Teste da remoção recursiva
+        System.out.println("\nREMOVENDO 30 - RECURSIVO:");
+        raiz = Exercicios.removerRecursivo(raiz, 30);
+        Exercicios.diagrama(raiz);
+
+
+        //Teste da remoção iterativa
+        System.out.println("\nREMOVENDO 70 - ITERATIVO:");
+        raiz = Exercicios.removerIterativo(raiz, 70);
+        Exercicios.diagrama(raiz);
+    }
+
+}
